9a5790e18128f40e641afa8fcdb35ad2d63b7569
Amir Madoie <amir7979@yahoo.com>
2024-09-24 22:16:55 +0330

    build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,15 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Link } from "@radix-ui/themes";
 
 const IssuesPage: React.FC = () => {
-  return <div><Button>New Issue</Button></div>;
+  return (
+    <div>
+      <Button>
+        <Link href="/issues/new" className="text-white">
+          New Issue
+        </Link>
+      </Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
