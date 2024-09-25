87b58f09ec437ef28326bb3f0c203e95a9fcb856
Amir Madoie <amir7979@yahoo.com>
2024-09-25 23:42:13 +0330

    Improve the loading expriense

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,10 @@
-import IssueForm from "../components/IssueForm";
+import dynamic from "next/dynamic";
 
-const NewIssuePage: React.FC  = () => {
+const IssueForm = dynamic(() => import("@/app/issues/components/IssueForm"), {
+  ssr: false,
+});
+
+const NewIssuePage: React.FC = () => {
   return <IssueForm />;
 };
 
