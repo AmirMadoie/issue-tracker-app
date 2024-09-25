f11659ccb38af7aab064f7fbd267e727c9bc5689
Amir Madoie <amir7979@yahoo.com>
2024-09-25 09:07:09 +0330

    implement clientSide validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,26 +1,35 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField, Text } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage: React.FC = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
   return (
     <div className="max-w-xl">
-     {error && <Callout.Root color="red" className="mb-5">
-        <Callout.Text>{error}</Callout.Text>
-      </Callout.Root>}
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
       <form
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
@@ -33,6 +42,7 @@ const NewIssuePage: React.FC = () => {
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
+        {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
         <Controller
           name="description"
           control={control}
@@ -40,6 +50,10 @@ const NewIssuePage: React.FC = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">{errors.description.message}</Text>
+        )}
+
         <Button>Submit New Issue</Button>
       </form>
     </div>
